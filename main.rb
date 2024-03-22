9d4692c5358b113a22c6e7d5ce065d2f33d4d931
Guilherme Guimarães <101267835+sggui@users.noreply.github.com>
2024-03-22 01:48:02 -0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,13 +2,13 @@
 require 'open-uri' #para abrir URLs
 
 class Extrator
-  @tipo = ""      # Tipo do endereço (ex: rua, avenida)
-  @nome = ""      # Nome do logradouro
-  @nume = ""      # Número do endereço
-  @comp = ""      # Complemento do endereço
-  @bairro = ""    # Bairro
-  @cidade = ""    # Cidade
-  @estado = ""    # Estado
+  @tipo = ""      # tipo do endereço (av, rua, travessa)
+  @nome = ""      # nome do logradouro
+  @nume = ""      # número do endereço
+  @comp = ""      # complemento do endereço
+  @bairro = ""    # bairro
+  @cidade = ""    # cidade
+  @estado = ""    # estado
   @cep = ""       # CEP
 
   def initialize(input)
@@ -36,12 +36,18 @@ class Extrator
     sub = @endereco.match(regex).to_s
     @endereco = @endereco.gsub(regex, "")
 
-    # extrair o tipo de logradouro
-    regex = /(((A|a)venida|(A|a)v)|((R|r)odovia)|((R|r)ua|^(\s*(R|r)))|(E|e)strada|(T|t)ravessa)(\.)?(\s)/
-    @tipo = sub.match(regex).to_s
-    sub = sub.gsub(regex,"")
-
     # extrair o numero do endereco
     regex = /(\d+(\w)?)$/
     @nume = sub.match(regex).to_s
     sub = sub.gsub(regex, "")
+
+    # extrair o tipo de logradouro
+    regex = /(((A|a)venida|(A|a)v)|((R|r)odovia)|((R|r)ua|^(\s*(R|r)))|(E|e)strada|(T|t)ravessa)(\.)?(\s)/
+    @tipo = sub.match(regex).to_s
+    sub = sub.gsub(regex,"")
+    
+    # extrair o nome do logradouro
+    regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s\.]+/
+    @nome = sub.match(regex).to_s
+    sub = sub.gsub(regex,"")
+  end
