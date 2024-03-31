cf8ff8fb0c33aa97b99f66460de3585763228e2a
Guilherme Guimarães <101267835+sggui@users.noreply.github.com>
2024-03-31 17:05:36 -0300

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,120 +1,118 @@
-#Extrator de endereços
-require 'open-uri' #para abrir URLs
+# Extrator de endereços
+require 'open-uri' # para abrir URLs
 
 class Extrator
-  @tipo = ""
-  @nome = ""
-  @nume = ""
-  @comp = ""
-  @bairro = ""
-  @cidade = ""
-  @estado = ""
-  @cep = ""
+  @tipo = ''
+  @nome = ''
+  @nume = ''
+  @comp = ''
+  @bairro = ''
+  @cidade = ''
+  @estado = ''
+  @cep = ''
 
   def initialize(input)
     @endereco = input
 
-    @tipo = "N/A"
-    @nome = "N/A"
-    @nume = "S/N"
-    @comp = "N/A"
-    @bairro = "N/A"
-    @cidade = "N/A"
-    @estado = "N/A"
-    @cep = "N/A"
+    @tipo = 'N/A'
+    @nome = 'N/A'
+    @nume = 'S/N'
+    @comp = 'N/A'
+    @bairro = 'N/A'
+    @cidade = 'N/A'
+    @estado = 'N/A'
+    @cep = 'N/A'
   end
 
   # Pra tirar as pontuações aleatórias que sobram no começo da string dps do gsub
   def limparComeco(input)
-    return input.to_s.gsub(/^(\.?\,?\-?\s?)/, "")
+    input.to_s.gsub(/^(\.?,?-?\s?)/, '')
   end
 
   def submatch
-    regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s\.]+( |\,) ?\d+(\w)?/
+    regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s.]+( |,) ?\d+(\w)?/
     sub = @endereco.match(regex).to_s
-    @endereco = @endereco.gsub(regex, "")
+    @endereco = @endereco.gsub(regex, '')
 
     regex = /(((A|a)venida|(A|a)v)|((R|r)odovia)|((R|r)ua|^(\s*(R|r)))|(E|e)strada|(T|t)ravessa)(\.)?(\s)/
     @tipo = sub.match(regex).to_s
-    sub = sub.gsub(regex,"")
+    sub = sub.gsub(regex, '')
 
     regex = /(\d+(\w)?)$/
     @nume = sub.match(regex).to_s
-    sub = sub.gsub(regex, "")
+    sub = sub.gsub(regex, '')
 
-    regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s\.]+/
+    regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s.]+/
     @nome = sub.match(regex).to_s
-    sub = sub.gsub(regex,"")
+    sub = sub.gsub(regex, '')
   end
 
   def complemento(input, regex)
     verif = input.match(/(bloco|bc|ap(to|artamento)|ala|andar|anexo|casa|cobertura|frente|fundos|port(a|ã)o|pr(e|é)dio|quadra|sala|sobrado|sobreloja|subsolo|t(e|é)rreo)+/i)
     if !verif.nil? && !verif.to_s.empty?
-      if @comp.to_s != "N/A"
-        @comp = @comp + ", " + input
-      else
-        @comp = input
-      end
-      @endereco = @endereco.gsub(regex, "")
+      @comp = if @comp.to_s != 'N/A'
+                @comp + ', ' + input
+              else
+                input
+              end
+      @endereco = @endereco.gsub(regex, '')
       return true
     end
-    return false
+    false
   end
 
   def bairro
-    regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s\.]+/i
+    regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s.]+/i
     @endereco = limparComeco(@endereco)
     @bairro = @endereco.match(regex)
-    @endereco = @endereco.gsub(regex, "")
-    if @bairro.to_s.to_i != 0
-      if @comp.to_s != "N/A"
-        @comp = @comp + ", " + @bairro.to_s
-      else
-        @comp = @bairro
-      end
-      bairro
-    end
+    @endereco = @endereco.gsub(regex, '')
+    return unless @bairro.to_s.to_i != 0
+
+    @comp = if @comp.to_s != 'N/A'
+              @comp + ', ' + @bairro.to_s
+            else
+              @bairro
+            end
+    bairro
   end
 
   def cidade
-    regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s\.]+/i
+    regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s.]+/i
     @endereco = limparComeco(@endereco)
     @cidade = @endereco.match(regex)
-    @endereco = @endereco.gsub(regex, "")
-    if @cidade.nil? || @cidade.to_s.empty?
-      @cidade = @bairro
-      @bairro = "N/A"
-    end
+    @endereco = @endereco.gsub(regex, '')
+    return unless @cidade.nil? || @cidade.to_s.empty?
+
+    @cidade = @bairro
+    @bairro = 'N/A'
   end
 
   def estado
     regex = /(AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO)/
     @estado = @endereco.match(regex)
-    @endereco = @endereco.gsub(regex, "")
+    @endereco = @endereco.gsub(regex, '')
   end
 
   def cep
     regex = /\d{5}(-| )?\d{3}/
     @cep = @endereco.match(regex)
-    if @cep.nil? || @cep.to_s.empty?
-      @cep = "N/A"
-    end
-    @endereco = @endereco.gsub(regex, "")
+    @cep = 'N/A' if @cep.nil? || @cep.to_s.empty?
+    @endereco = @endereco.gsub(regex, '')
   end
 
   def show
-    puts "Tipo: " + @tipo.to_s
-    puts "Nome: " + @nome.to_s
-    puts "Numero: " + @nume.to_s
-    puts "Complemento: " + @comp.to_s
-    puts "Bairro: " + @bairro.to_s
-    puts "Cidade: " + @cidade.to_s
-    puts "Estado: " + @estado.to_s
-    puts "CEP: " + @cep.to_s
+    puts 'Tipo: ' + @tipo.to_s
+    puts 'Nome: ' + @nome.to_s
+    puts 'Numero: ' + @nume.to_s
+    puts 'Complemento: ' + @comp.to_s
+    puts 'Bairro: ' + @bairro.to_s
+    puts 'Cidade: ' + @cidade.to_s
+    puts 'Estado: ' + @estado.to_s
+    puts 'CEP: ' + @cep.to_s
   end
 
   def opcionais
-    regexComp = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s\.]+(?=\,|\-)/
+    regexComp = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s.]+(?=,|-)/
     bat = true
     while bat
       @endereco = limparComeco(@endereco)
@@ -126,9 +124,9 @@ class Extrator
   end
 
   def extract
-    print("Endereço encontrado: ", @endereco, "\n")
+    print('Endereço encontrado: ', @endereco, "\n")
 
-    initial_matches = @endereco.scan(/[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s\.]+( |\,) ?\d+(\w)?/)
+    initial_matches = @endereco.scan(/[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s.]+( |,) ?\d+(\w)?/)
     initial_count = initial_matches.size
 
     submatch
@@ -139,32 +137,32 @@ class Extrator
     puts
     show
 
-    final_matches = @endereco.scan(/[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s\.]+( |\,) ?\d+(\w)?/)
+    final_matches = @endereco.scan(/[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s.]+( |,) ?\d+(\w)?/)
     final_count = final_matches.size
 
     removed_count = initial_count - final_count
     print("\nQtd matches removidos: #{removed_count}\n")
     print("matches removidos:\n", @endereco)
-    end
   end
+end
 
 class Programa
   def iniciar
-    puts "|---------------------|"
-    puts "|   EP02 - LFA        |"
-    puts "|                     |"
-    puts "| Guilherme Guimarães |"
-    puts "| &                   |"
-    puts "| Gustavo Piroupo     |"
+    puts '|---------------------|'
+    puts '|   EP02 - LFA        |'
+    puts '|                     |'
+    puts '| Guilherme Guimarães |'
+    puts '| &                   |'
+    puts '| Gustavo Piroupo     |'
     puts "|---------------------|\n\n"
-    puts "Escolha qual opção você quer usar no nosso projeto:"
-    puts "1 - Ler uma URL pré-definida", "2 - Ler um texto pré-definido", "3 - Inserir um texto", "4 - Inserir uma URL"
-    puts "Sua escolha: "
+    puts 'Escolha qual opção você quer usar no nosso projeto:'
+    puts '1 - Ler uma URL pré-definida', '2 - Ler um texto pré-definido', '3 - Inserir um texto', '4 - Inserir uma URL'
+    puts 'Sua escolha: '
     while true
       escolha = gets.chomp
       case escolha.to_i
       when 1
-        lerURI
+        lerURL
         break
       when 2
         lerTexto
@@ -176,68 +174,66 @@ class Programa
         usuarioInsereUrl
         break
       else
-        puts "Selecione um valor válido:"
+        puts 'Selecione um valor válido:'
       end
     end
   end
 
-  def lerURI
+  def lerURL
     system 'clear'
-    puts "Escolha a URL que você gostaria de ler:"
-    puts "1 - SENAC", "2 - Mini Carta 2", "3 - Nava Technology for Business", "4 - Shopping Leblon"
-    puts "Selecione o número da sua escolha: "
+    puts 'Escolha a URL que você gostaria de ler:'
+    puts '1 - SENAC', '2 - Mini Carta 2', '3 - Nava Technology for Business', '4 - Shopping Leblon'
+    puts 'Selecione o número da sua escolha: '
 
-    file = File.open("texto.txt")
+    file = File.open('texto.txt')
     texto = file.readlines.map(&:chomp)
-    link = ""
+    link = ''
     while true
       escolha = gets.chomp
       case escolha.to_i
       when 1
-        link = texto[5]
+        link = texto[4]
         break
       when 2
-        link = texto[7]
+        link = texto[6]
         break
       when 3
-        link = texto[9]
+        link = texto[8]
         break
       when 4
-        link = texto[11]
+        link = texto[10]
         break
       else
-        puts "Selecione um valor válido:"
+        puts 'Selecione um valor válido:'
       end
     end
     file.close
-    uri = URI.parse(link)
-    data = uri.read # string que contem todo o texto do site.
+    data = URI.open(link).read
     extrair(data)
   end
 
   def lerTexto
     system 'clear'
-    puts "O texto é o seguinte:"
-    file = File.open("texto.txt")
+    puts 'O texto é o seguinte:'
+    file = File.open('texto.txt')
     texto = file.readlines.map(&:chomp)
     file.close
     puts texto[0]
-    puts texto[1]
-    puts ""
-    extrair(texto[1])
+    puts ''
+    extrair(texto[0])
   end
 
   def usuarioInsereTxt
     system 'clear'
-    puts "Por favor, insira seu texto (O endereço deve estar nesse formato: Rua|Avenida, nº - Bairro, Cidade - Estado (sigla), CEP (00000-000)):"
+    puts 'Por favor, insira seu texto (O endereço deve estar nesse formato: Rua|Avenida, nº - Bairro, Cidade - Estado (sigla), CEP (00000-000)):'
     texto = gets.chomp
-    puts ""
+    puts ''
     extrair(texto)
   end
 
   def usuarioInsereUrl
     system 'clear'
-    puts "obs.: Pode não funcionar em todos os sites", "Por favor, insira seu link:"
+    puts 'obs.: Pode não funcionar em todos os sites', 'Por favor, insira seu link:'
     link = gets.chomp
 
     uri = URI.parse(link)
@@ -247,12 +243,12 @@ class Programa
   end
 
   def extrair(texto)
-  regex = /\b(((A|a)venida|(A|a)v)|((R|r)odovia)|((R|r)ua|(R|r))|(E|e)strada|(T|t)ravessa)\.?\s([A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9 \.\,\-\/\|\:]+)\s(((AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO))|(\d{5}(\-| )\d{3}))\b/
+    regex = %r{\b(((A|a)venida|(A|a)v)|((R|r)odovia)|((R|r)ua|(R|r))|(E|e)strada|(T|t)ravessa)\.?\s([A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9 .,\-/|:]+)\s(((AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO))|(\d{5}(-| )\d{3}))\b}
 
-  textoR = texto.match(regex).to_s
-  textoR = textoR.gsub(/>|</,"")
-  endereco = Extrator.new(textoR)
-  endereco.extract
+    textoR = texto.match(regex).to_s
+    textoR = textoR.gsub(/>|</, '')
+    endereco = Extrator.new(textoR)
+    endereco.extract
   end
 end
 
